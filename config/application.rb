5635eacd366454c8205d5210b1276061f9b8132d
tone0623 <tone0623@icloud.com>
2022-06-11 04:53:43 +0000

    config設定/.env設定

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Bookers2Ouyou6
+module DeployBookers2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
